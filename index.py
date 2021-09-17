57d74f5745c30509b223ae8b5bee9294b3bdb296
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-09-17 17:31:43 -0400

    first commit

---
+++ index.py
@@ -0,0 +1,19 @@
+'''
+Disciplina: Criptografia e Segurança de Dados
+Docente: Nelcileno Araújo
+Discente: Wanderson Timóteo
+RGA: 201511316040
+
+Atividade: Cifra de Playfair
+O programa deve fornecer um interface com o usuário através de um menu, com (pelo menos) as seguintes opções:
+1 - Escolher uma tabela de cifra nova
+2 - Introduzir uma mensagem para cifrar (de um arquivo ou do stdin)
+3 - Ver a mensagem cifrada
+4 - Decifrar a mensagem
+5 - Ver o alfabeto
+6 - Terminar
+
+Poderá ainda permitir a escolha do alfabeto ou o formato da tabela de cifra.
+
+'''
+
